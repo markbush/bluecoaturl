68f8b639281f513a5e38e32bd2cff8ea77f564aa
mark <mark@bushnet.org>
2007-10-17 21:35:43 +0000

    Deployment scripting support

---
+++ lib/deployer.rb
@@ -0,0 +1,17 @@
+class Deployer
+  def self.deploy *hostnames
+    hosts = Host.find(:all)
+    hosts.each do |host|
+      if hostnames.empty? || hostnames.include?(host.hostname)
+        puts "Calling: host.deploy for #{host.hostname}"
+        begin
+          res = host.deploy
+          puts "Result:"
+          puts res
+        rescue Exception => e
+          puts "Failed to deploy to #{host.hostname}: #{e.message}"
+        end
+      end
+    end
+  end
+end
