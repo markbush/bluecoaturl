8ed024ba89fb981ddf748eb4e749d9e54b3fc1b8
mattwestby <mattwestby@80ecddd5-4e38-0410-9614-9325593040c7>
2007-10-17 22:53:38 +0000

    extend host class to include testurl

--- trunk/db/migrate/007_add_initial_user.rb
+++ trunk/db/migrate/007_add_initial_user.rb
@@ -1,8 +1,5 @@
 class AddInitialUser < ActiveRecord::Migration
   def self.up
-    user = User.new(:name => 'admin', :password => 'admin', :password_confirmation => 'admin')
-    user.admin = true
-    user.save
   end
 
   def self.down
