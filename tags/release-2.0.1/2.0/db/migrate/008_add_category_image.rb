6bf0360c5738f229c5d4a68a3f9c54b72659fc3c
mark@bushnet.org <mark@bushnet.org@80ecddd5-4e38-0410-9614-9325593040c7>
2009-06-07 20:56:27 +0000

    Repository repair

--- tags/release-2.0.1/2.0/db/migrate/008_add_category_image.rb
+++
@@ -1,9 +0,0 @@
-class AddCategoryImage < ActiveRecord::Migration
-  def self.up
-    add_column :categories, :image, :binary
-  end
-
-  def self.down
-    remove_column :categories, :image
-  end
-end
--- tags/release-2.0.1/db/migrate/011_add_user_can_deploy.rb
+++
@@ -1,9 +0,0 @@
-class AddUserCanDeploy < ActiveRecord::Migration
-  def self.up
-    add_column :users, :can_deploy, :boolean, :default => true
-  end
-
-  def self.down
-    remove_column :users, :can_deploy
-  end
-end
